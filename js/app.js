205e153622455e8c7098b80d083e3c8797ca5a65
Troy Coffindaffer <troy.coffindaffer@gmail.com>
2021-11-01 16:22:42 -0700

    Put search.addEventListener method into a function

--- js/app.js
+++ js/app.js
@@ -1,1 +1,19 @@
 baguetteBox.run('.photo-gallery');
+
+const searchField = document.querySelector('#search');
+const cards = document.getElementsByTagName('a');
+
+function handleSearchOnKeyUp() {
+  searchField.addEventListener('keyup', () => {
+    for (let i = 0; i < cards.length; i++) {
+      cards[i].style.display = 'none';
+    }
+    for (let i = 0; i < cards.length; i++) {
+      if (cards[i].getAttribute('data-caption').toLowerCase().includes(searchField.value)) {
+        cards[i].style.display = 'inline';
+      }
+    }
+  });
+}
+
+handleSearchOnKeyUp();
